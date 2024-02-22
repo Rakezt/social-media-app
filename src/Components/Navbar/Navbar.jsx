e316e6d0a235ec66a1b64ae28e3734313a5676df
L Rakesh Singh <110081692+Rakezt@users.noreply.github.com>
2024-02-22 11:35:54 +0530

    update logo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import icon from "../Assets/images/icon.png";
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center border-b border-gray-100 w-full px-44 py-2">
-      <img className="h-8 mr-4" src={icon} alt="logo" />
+      <img src={icon} className="App-logo h-8 mr-4" alt="logo" />
       <Link to="/">
         <div className="text-3xl font-extrabold text-gray-900 dark:text-white font-roboto">
           <span className="text-transparent bg-clip-text bg-blue-400">
